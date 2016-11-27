e276fa2844ecf7c351923221b0e682dae2c61f2d
Jake <jnelson180@gmail.com>
2016-11-27 16:01:22 -0600

    Ran JS beautifier

--- metafile.js
+++ metafile.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 8080;
 var multer = require('multer');
 // var upload = multer({ dest: 'uploads/' });
 // dest removed so as not to permanently save files to app server
-var upload = multer({ }); 
+var upload = multer({});
 
 // set up view engine and static asset directory
 app.set('view engine', 'ejs');
@@ -12,18 +12,20 @@ app.use(express.static(__dirname + '/public'));
 
 // serve home page
 app.get('/', function(req, res) {
-	console.log("Visitor to homepage.");
-	res.render('index');
-	res.end();
+    console.log("Visitor to homepage.");
+    res.render('index');
+    res.end();
 });
 
-app.post('/filedata', upload.single('userFile'), function (req, res) {
-  // req.body would hold the text fields, if there were any
-  // but req.file is used instead (contains multer data) 
-  console.log(req.file);
-  res.send(JSON.stringify({ "size": req.file.size }));
+app.post('/filedata', upload.single('userFile'), function(req, res) {
+    // req.body would hold the text fields, if there were any
+    // but req.file is used instead (contains multer data) 
+    console.log(req.file);
+    res.send(JSON.stringify({
+        "size": req.file.size
+    }));
 });
 
 app.listen(port, function() {
-	console.log("File data microservice running at " + port)
+    console.log("File data microservice running at " + port)
 });
